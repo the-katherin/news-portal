882c0079766258c38cd2abd2ad20e8d94002d1b8
Katerina Tsiurikova <ekaterina082612@gmail.com>
2018-11-14 11:14:32 +0300

    add channels and news rendering functions

--- app/app.js
+++ app/app.js
@@ -1,30 +1,128 @@
 import './styles/main.scss';
 
-import getNews from './js/newsApiService'
+import { urlForEverything, urlForSources, getData } from './js/newsApiService';
 
-async function loadNews () {
-    const {articles} = await getNews();
+const doc = document;
+const newsContainer = doc.getElementById('news-list');
+const channelsContainer = doc.getElementById('channels-list');
+const chooseChannelButton = doc.getElementById('choose-channel-button');
+const CHANNELS_LIST_POSITION_Y = 40;
+const append = (parent, el) => parent.appendChild(el);
+const createNode = element => doc.createElement(element);
+
+// todo split to different functions or move to service repo
+async function loadNews() {
+    const { articles } = await getData(urlForEverything);
+    insertArticles(articles);
     console.log(articles);
     console.log('done');
 }
 
+async function loadSources() {
+    const { sources } = await getData(urlForSources);
+    renderChannels(sources);
+    console.log(sources);
+    console.log('done');
+    // parseDate();
+}
+
 loadNews();
 
-// var url = 'https://newsapi.org/v2/top-headlines?' +
-//     'country=us&' +
-//     'apiKey=02e8d88534de4f6abb6c90514e95aaea';
-// var req = new Request(url);
+loadSources();
 
-// fetch(req)
-//     .then(response => {
-//         console.log(response.json())}
-//     );
+const onChooseChannelButtonClick = e => {
+    scrollTo(0, CHANNELS_LIST_POSITION_Y);
+    channelsContainer.classList.toggle('channels-list--show');
+    console.log('clicked');
+};
 
-// fetch(req)
-//     .then(response => {
-//         console.log(response.json());
-//     });
+const onChooseChannel = e => {
+    const { target } = e;
+    const channelId = target.id || target.parentElement.id;
+    console.log(target.id);
+    console.log(target.parentElement.id); //
+    console.log(channelId);
+};
+
+const renderChannels = channels => {
+    channels.map(channel => {
+        const {
+            category,
+            description,
+            id,
+            name,
+            url,
+        } = channel;
+
+        const channelItem = createNode('div');
+        channelItem.classList.add('channel');
+        channelItem.setAttribute('id', `${id}`);
+
+        channelItem.innerHTML =
+            `
+                <h3 class="channel__name">${name}</h3>
+                <span class='channel__category'>${category}</span>
+                <p class="channel__description">${description}</p>
+                 <a href=${url} target='_blank'>Link</a>
+            `
+        append(channelsContainer, channelItem);
+
+    });
+};
+
+
+chooseChannelButton.addEventListener('click', onChooseChannelButtonClick);
+
+channelsContainer.addEventListener('click', onChooseChannel);
 
 
 
-console.log('work');
+const insertArticles = articles => {
+    articles.map(article => {
+        const {
+            author,
+            content,
+            description,
+            publishedAt,
+            source, //todo name //id
+            title,
+            url,
+            urlToImage,
+        } = article;
+
+        const newsItem = createNode('div');
+        newsItem.classList.add('news-list__item', 'news');
+
+        const image = createNode('img'); //todo move to inner html
+        image.classList.add('news__image');
+        image.src = urlToImage;
+
+        const newsItemContent = createNode('div');
+        newsItemContent.classList.add('news__content');
+
+        newsItemContent.innerHTML =
+            `
+            <h2 class='news-title'>
+                <a href=${url} target='_blank'>${title}</a>
+            </h2>
+            <p class='news-description'>${description}</p>
+            <p classs='news-author'>Author: ${author}</p>
+            <div class='news-info'> 
+                <span class='news-date'>${new Date(publishedAt).toUTCString()} | </span>
+                <span className="news-source">${source.name}</span>
+            </div>
+            `;
+
+
+
+        append(newsItem, image);
+        append(newsItem, newsItemContent);
+        append(newsContainer, newsItem);
+
+    });
+}
+
+
+
+
+
--- app/js/newsApiService.js
+++ app/js/newsApiService.js
@@ -1,21 +1,22 @@
 const myApiKey = '02e8d88534de4f6abb6c90514e95aaea';
 const pageSize = 10;
-const newsApiUrl = 'https://newsapi.org/v2/top-headlines?';
-const country = 'us';
+const newsApiUrl = 'https://newsapi.org/v2/';
+const endpointForEverything = 'everything?';
+const endpointForSources = 'sources?';
+const country = 'us'; // todo ? delete
+const language = 'en';
 
-const url = `${newsApiUrl}country=${country}&pageSize=${pageSize}&apiKey=${myApiKey}`;
+const urlForEverything = `${newsApiUrl}${endpointForEverything}q=news&language=${language}&pageSize=${pageSize}&apiKey=${myApiKey}`;
 
-var customUrl = 'https://newsapi.org/v2/top-headlines?' +
-    'country=us&' +
-    'pageSize=10&' +
-    'apiKey=02e8d88534de4f6abb6c90514e95aaea';
-const req = new Request(url);
+const urlForSources = `${newsApiUrl}${endpointForSources}language=${language}&country=${country}&apiKey=${myApiKey}`;
 
-async function getNews () {
+async function getData(url) {
+
+    const req = new Request(url);
 
     return fetch(req)
-        .then(response => response.json() )
-        .catch(error =>  console.log(error));
+        .then(response => response.json())
+        .catch(error => console.log(error));
 }
 
-export default getNews;
+export { urlForEverything, urlForSources, getData };

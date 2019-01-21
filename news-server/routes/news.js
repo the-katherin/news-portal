8aac504d42b21d5e2068396e466a93983b90f6a3
Katerina <katsiaryna_tsiurykava@epam.com>
2019-01-21 20:45:03 +0300

    implement CRUD operations in news router

--- news-server/routes/news.js
+++ news-server/routes/news.js
@@ -1,11 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-const newsJson = require('../public/data/news.json');
+const newsJSON = require('../data/news.json');
+let newsArray = newsJSON;
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.json(newsJson); // todo render layout or users
+/* GET news listing. */
+router.get('/', function (req, res) {
+  res.json(newsArray); // todo render layout
+});
+
+/* POST new news item */
+
+router.post('/', function (req, res) {
+  const { payload, title } = req.body;
+  const news = {
+    id: Date.now(),
+    title,
+    payload,
+  };
+
+  newsArray.push(news);
+  res.send(newsArray);
+});
+
+/* PUT payload data to specified with ID news item */
+
+router.put('/:id', function (req, res) {
+  const { id } = req.params;
+  const { payload } = req.body;
+  const news = newsArray.find(newsItem => newsItem.id === id);
+
+  if (news) {
+    news.payload = payload;
+  };
+
+  res.send(newsArray);
+});
+
+/* DELETE specified with ID news item */
+
+router.delete('/:id', function (req, res) {
+  const { id } = req.params;
+  const newsIndex = newsArray.findIndex(newsItem => newsItem.id === id);
+
+  if (newsIndex > -1) {
+    newsArray.splice(newsIndex, 1);
+  };
+
+  res.send(newsArray);
 });
 
 module.exports = router;

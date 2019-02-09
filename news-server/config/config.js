11c36e0307a22b3ede4506d72a620781511e35c6
Katerina <katsiaryna_tsiurykava@epam.com>
2019-02-09 17:40:40 +0300

    generate basic angular app

--- news-server/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: 'mongodb://127.0.0.1/lesson8',
-    secret: 'yoursecret',
-    facebook: {
-        id: "376966159518886",
-        secretKey: 'acea8c68dfe5a81d39c0080b448735ad',
-    }
-};

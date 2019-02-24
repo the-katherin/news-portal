64b40e886ec1909e922952d61c64db39e20f5a78
Katerina <katsiaryna_tsiurykava@epam.com>
2019-02-24 21:47:32 +0300

    11_unit_tests_angular // add tests for title service

--- news-app/src/app/services/title.service.spec.ts
+++ news-app/src/app/services/title.service.spec.ts
@@ -1,12 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { TitleService } from './title.service';
 
-describe('TitleService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('TitleService', () => {
+    const stubTitle = 'Title';
+    let injector: TestBed;
+    let service: TitleService;
 
-  it('should be created', () => {
-    const service: TitleService = TestBed.get(TitleService);
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [TitleService],
+        });
+
+        injector = getTestBed();
+        service = injector.get(TitleService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    describe('onChangeTitle', () => {
+        it('should set title and emit changeTitle function', () => {
+            spyOn(service.changeTitle, 'emit');
+
+            service.onChangeTitle(stubTitle);
+
+            expect(service.title).toBe(stubTitle);
+            expect(service.changeTitle.emit).toHaveBeenCalledWith(stubTitle);
+        });
+    });
 });
